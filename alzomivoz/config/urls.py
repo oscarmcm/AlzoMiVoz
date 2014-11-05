1e2bc75bd1ef5a92745c9439c05979958d8c07f5
Cajetero <om.cortez.2010@gmail.com>
2014-11-05 17:21:53 -0600

    changed urls and solved error with the index

--- alzomivoz/config/urls.py
+++ alzomivoz/config/urls.py
@@ -16,6 +16,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),   
     url(r'^/about/$', TemplateView.as_view(template_name="about.html")),
     url(r'^denuncia/', include('denuncia.urls')),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
 ) 
 
 
